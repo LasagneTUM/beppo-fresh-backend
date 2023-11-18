9ac57af10646a202a12c44e1703dcc5e9a31f821
Niclas Huelsmann <niclas.huelsmann@tum.de>
2023-11-18 21:10:28 +0100

    fix cosine similarity

--- app/ranking.py
+++ app/ranking.py
@@ -2,10 +2,10 @@ from typing import List, Tuple
 
 from models.models import UserPreference, Recipe
 
-import sklearn
+from sklearn.metrics.pairwise import cosine_similarity
 
 def calculate_score(lst1: List[float], lst2: List[float]) -> float:
-    return sklearn.metrics.pairwise.cosine_similarity(lst1, lst2)
+    return cosine_similarity(lst1, lst2)
 
 def rank(user: UserPreference, recipes: List[Recipe]) -> List[Recipe]:
     ranked : List[Tuple[Recipe, float]] = []
