41fb500d97ad96f23601b84780fd74b7ce30e307
Niclas Huelsmann <niclas.huelsmann@tum.de>
2023-11-18 21:19:31 +0100

    fix ranking maybe

--- app/ranking.py
+++ app/ranking.py
@@ -10,6 +10,6 @@ def calculate_score(lst1: List[float], lst2: List[float]) -> float:
 def rank(user: UserPreference, recipes: List[Recipe]) -> List[Recipe]:
     ranked : List[Tuple[Recipe, float]] = []
     for r in recipes:
-        ranked.append((r, calculate_score(user.preferences, r.vector)))
+        ranked.append((r, calculate_score([user.preferences], [r.vector])))
     ranked.sort(key=lambda x: x[1], reverse=True)
     return [r[0] for r in ranked]
