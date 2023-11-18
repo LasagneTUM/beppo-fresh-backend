1fb980aab86f0ab2c7b8fd8b3a11d04fdfd9bde8
Niclas Huelsmann <niclas.huelsmann@tum.de>
2023-11-18 12:04:11 +0100

    routes for recommendations and ingredients

--- app/main.py
+++ app/main.py
@@ -1,14 +1,31 @@
 from typing import Union
+import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 fastapi_app = FastAPI()
 
+with open("data/mockedIngredients.json") as f:
+    ingredients = json.load(f)["ingredients"]
+
+with open("data/mockedRecipes.json") as f:
+    recipes = json.load(f)
+    recipes = [i['recipe'] for i in recipes]
+
 @fastapi_app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
-
 @fastapi_app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@fastapi_app.get("/recommendations")
+async def recommendations(request: Request):
+    user = request.headers.get("user")
+    return recipes
+
+@fastapi_app.get("/ingredients")
+async def get_ingredients():
+    return ingredients
+
