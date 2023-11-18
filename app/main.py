b851e09b72a25953056a1d45df4bbe9cddf54c5c
Niclas Huelsmann <niclas.huelsmann@tum.de>
2023-11-18 10:37:36 +0100

    add test and new requirements.txt

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from typing import Union
 
 from fastapi import FastAPI
 
-app = FastAPI()
+fastapi_app = FastAPI()
 
 
-@app.get("/")
+@fastapi_app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@fastapi_app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
