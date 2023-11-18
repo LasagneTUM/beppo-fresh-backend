6fe08c5ce7807afc069ba04b1b742eb69881b0b9
Niclas Huelsmann <niclas.huelsmann@tum.de>
2023-11-18 20:50:34 +0100

    load data

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from models.models import UserPreferenceUpdate, Ingredient, Recipe, Option
 
-from storage import add_multiple_preferences, get_preference_or_create, all_recipes
+from storage import add_multiple_preferences, get_preference_or_create, all_recipes, save_recipe_data, recipe_collection, preference_collection
 
 from .ranking import rank
 
@@ -24,12 +24,16 @@ fastapi_app.add_middleware(
     allow_headers=["*"],
 )
 
+preference_collection.delete_many({})
+recipe_collection.delete_many({})
+
 with open("data/mockedIngredients.json") as f:
     ingredients = json.load(f)["ingredients"]
 
 with open("data/mockedRecipes.json") as f:
     recipes = json.load(f)
     recipes = [i['recipe'] for i in recipes]
+    save_recipe_data(recipes)
 
 options = [
     Option(
