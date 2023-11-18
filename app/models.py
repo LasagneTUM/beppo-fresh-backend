60b389b05b838071276c4f37921c3c004698dfd8
Niclas Huelsmann <niclas.huelsmann@tum.de>
2023-11-18 15:11:07 +0100

    added docker compose

--- app/models.py
+++ app/models.py
@@ -1,5 +1,30 @@
+from typing import List
 from pydantic import BaseModel
 
 class UserPreference(BaseModel):
     user: str
     preference: str
+
+class TagEntry(BaseModel):
+    name: str
+
+class NutritionInfo(BaseModel):
+    energy: int
+    calories: int
+    carbohydrate: float
+    protein: float
+
+class Recipe(BaseModel):
+    id: str
+    name: str
+    headline: str
+    prepTime: int
+    image: str
+    websiteURL: str
+    tags: List[TagEntry]
+    nutrition: NutritionInfo
+
+class Ingredient(BaseModel):
+    name: str
+    type: str
+    imageLink: str
