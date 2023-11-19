5f90807107f394b3dcac11815d6bd1bc528dffff
Alexander Liebald <alexander.liebald@gmail.com>
2023-11-19 01:12:26 +0100

    Add log_requests middleware

--- app/main.py
+++ app/main.py
@@ -36,6 +36,22 @@ with open("data/mockedRecipes.json") as f:
     recipes = [i['recipe'] for i in recipes]
     save_recipe_data(recipes)
 
+logger = logging.getLogger(__name__)
+
+@fastapi_app.middleware("http")
+async def log_requests(request: Request, call_next):
+    idem = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    logger.info(f"rid={idem} start request path={request.url.path}")
+    start_time = time.time()
+
+    response = await call_next(request)
+
+    process_time = (time.time() - start_time) * 1000
+    formatted_process_time = '{0:.2f}'.format(process_time)
+    logger.info(f"rid={idem} completed_in={formatted_process_time}ms status_code={response.status_code}")
+
+    return response
+
 def get_ranked(user) -> List[Recipe]:
     user_obj = get_preference_or_create(cast(str, user))
     recipes = all_recipes()
